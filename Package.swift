bcba592983159fc5aa3fa420741a640dc61a4604
Max Nguyen <max.nguyen@techvify-dn.com>
2023-04-24 13:09:29 +0700

    Update lint

--- Package.swift
+++ Package.swift
@@ -4,29 +4,29 @@
 import PackageDescription
 
 let package = Package(
-    name: "Common",
-    platforms: [
-      .iOS(.v13),
-      .macOS(.v10_15),
-      .tvOS(.v13),
-      .watchOS(.v6),
-    ],
-    products: [
-        .library(
-            name: "Common",
-            targets: ["Common"]),
-    ],
-    dependencies: [
-        .package(url: "https://github.com/kishikawakatsumi/KeychainAccess.git", branch: "master"),
-    ],
-    targets: [
-        .target(
-            name: "Common",
-            dependencies: [
-                .product(name: "KeychainAccess", package: "KeychainAccess"),
-            ]),
-        .testTarget(
-            name: "CommonTests",
-            dependencies: ["Common"]),
-    ]
+	name: "Common",
+	platforms: [
+		.iOS(.v13),
+		.macOS(.v10_15),
+		.tvOS(.v13),
+		.watchOS(.v6)
+	],
+	products: [
+		.library(
+			name: "Common",
+			targets: ["Common"])
+	],
+	dependencies: [
+		.package(url: "https://github.com/kishikawakatsumi/KeychainAccess.git", branch: "master"),
+	],
+	targets: [
+		.target(
+			name: "Common",
+			dependencies: [
+				.product(name: "KeychainAccess", package: "KeychainAccess")
+			]),
+		.testTarget(
+			name: "CommonTests",
+			dependencies: ["Common"])
+	]
 )
